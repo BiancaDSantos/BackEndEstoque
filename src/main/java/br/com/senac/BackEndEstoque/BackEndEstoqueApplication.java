45f662becba9d591287f8c66bdcdf18b2a363bc8
BiancaDSantos <biancadossantossc@gmail.com>
2024-06-21 02:38:52 -0300

    Teste

---
+++ src/main/java/br/com/senac/BackEndEstoque/BackEndEstoqueApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.BackEndEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senac/BackEndEstoque/BackEndEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.BackEndEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
